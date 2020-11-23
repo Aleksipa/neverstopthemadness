71f89f53e693a0ff2112685309ea84b5ddc3b727
Ossi <ossi.j.lehtonen@helsinki.fi>
2020-11-23 10:54:34 +0200

    config postgres as prod db and sqlite as dev db

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,21 @@
 from flask import Flask
 app = Flask(__name__)
 
+from flask_sqlalchemy import SQLAlchemy
+
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///local.db"
+    # This makes the app print all SQL-queries when ran locally
+    app.config["SQLALCHEMY_ECHO"] = True
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
+db = SQLAlchemy(app)
+
 from application import views
+
+
+db.create_all()
